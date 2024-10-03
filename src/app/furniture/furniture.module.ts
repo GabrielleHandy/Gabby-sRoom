f44d30875a46a9b2a110043182d2d24742b90843
Gabrielle Handy <gabrielle.handy@CVSHealth.com>
2024-10-03 17:03:14 -0400

    Added laptop and calendar base shapes

--- src/app/furniture/furniture.module.ts
+++ src/app/furniture/furniture.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableComponent } from './table/table.component';
 import { LampComponent } from './lamp/lamp.component';
+import { ComputerComponent } from './computer/computer.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 
 
@@ -10,8 +12,10 @@ import { LampComponent } from './lamp/lamp.component';
   imports: [
     CommonModule,
     TableComponent, 
-    LampComponent
+    LampComponent,
+    ComputerComponent,
+    CalendarComponent
   ],
-  exports:[TableComponent, LampComponent]
+  exports:[TableComponent, LampComponent, CalendarComponent, ComputerComponent]
 })
 export class FurnitureModule { }
